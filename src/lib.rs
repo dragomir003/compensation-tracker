b5ff35979107999e4a479e487d5a1fc698b63eb5
dragomir003 <dragolekovic@gmail.com>
2023-07-27 16:31:53 +0200

    Added docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,26 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![deny(missing_docs)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/*!
+# Compensation Tracker
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+This crate implements Compensation Tracker(\<insert-paper-here\>) as well as
+the so called 'Basic Tracker'.
+
+The coupling is done for 2 reasons:
+  - Ease of Implementation
+  - Possibly performance gain
+
+Anyway, this crate relies on following concepts:
+  - Detection - Something received from e.g. Neural Network and has no
+    information about itself aside from bounding box, confidence and classification
+  - Tracklet - The main idea behind this module. For now on surface level it is
+    simply a series of detections which keeps track of it's movements and classification
+  - Basic Tracker performing 'Motion Compensation' with a Kallman Filter and top
+    level assignment of detections to tracklets
+  - Compensation Tracker - A tracking approach designed to handle detector's
+    shortcomings and mutual tracklet occlusion
+
+*/
+
+
+
